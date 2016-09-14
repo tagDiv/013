e62756f40827460217b2774d3b3519881aa8f484
racoder <ra_coder@yahoo.com>
2016-09-14 15:22:05 +0300

    class- file rename

--- functions.php
+++ functions.php
@@ -7,7 +7,9 @@ require_once 'deploy-mode.php';
 require_once('includes/class-tagdiv-config.php');
 add_action('tagdiv_global_after', array('Tagdiv_Config', 'on_tagdiv_global_after_config'), 9); //we run on 9 priority to allow plugins to updage_key our apis while using the default priority of 10
 
-require_once('includes/wp-booster/wp_booster_functions.php');
+
+require_once('includes/wp-booster/wp-booster-transition.php');
+require_once('includes/wp-booster/wp-booster-functions.php');
 
 require_once('includes/tagdiv_css_generator.php');
 
